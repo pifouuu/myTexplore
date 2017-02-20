3e530e9f1abd6d2cd930fabaa5edafadacf19c30
pifouuu <pierre00fournier@gmail.com>
2017-02-20 15:50:47 +0100

    change gitignore

--- Debug/include/newmat/myexcept.d
+++
@@ -1,6 +0,0 @@
-include/newmat/myexcept.o: ../include/newmat/myexcept.cc \
- ../include/newmat/include.h ../include/newmat/myexcept.h
-
-../include/newmat/include.h:
-
-../include/newmat/myexcept.h:
--- Debug/src/common/Random.d
+++
@@ -1,4 +0,0 @@
-src/common/Random.o: ../src/common/Random.cc \
- ../src/common/../../include/common/Random.h
-
-../src/common/../../include/common/Random.h:
