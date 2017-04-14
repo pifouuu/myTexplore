9401a4d74aba3a6b40f58b3afbbbba053fc4d511
pifouuu <pierre00fournier@gmail.com>
2017-04-14 10:51:52 +0200

    change num trails

--- src/myRlExperiment.cpp
+++ src/myRlExperiment.cpp
@@ -54,8 +54,8 @@
 #include <getopt.h>
 #include <stdlib.h>
 
-unsigned NUMEPISODES = 10000; //10; //200; //500; //200;
-const unsigned NUMTRIALS = 1; //30; //30; //5; //30; //30; //50
+unsigned NUMEPISODES = 1000; //10; //200; //500; //200;
+const unsigned NUMTRIALS = 5; //30; //30; //5; //30; //30; //50
 unsigned MAXSTEPS = 100; // per episode
 bool PRINTS = false;
 bool PRETRAIN = false;
